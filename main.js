170c18fb65a0650f56c68e4b4e11ac5c426347b1
Dan Dorsey <Maerus1@users.noreply.github.com>
2015-02-08 13:49:28 -0500

    reworked references to the images in main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var pics = document.querySelector("#pics");
 
 var picArray = [];
-picArray[0] = "url('http://www.datishop.com.br/media/catalog/product/d/c/dcml1.jpg')";
-picArray[1] = "url('http://www.nvnews.net/articles/bioshock_gameplay/images/bioshock_rapture.png')";
-picArray[2] = "url('http://stylefavor.com/wp-content/uploads/2012/10/Bioshock_gameplay-screen.jpg')";
+picArray[0] = "url('dcml1.jpg')";
+picArray[1] = "url('bioshock_rapture.png')";
+picArray[2] = "url('Bioshock_gameplay-screen.jpg')";
 var increase = 0;
 pics.backgroundImage = picArray[increase];
 pics.addEventListener("click", clickHandler, false);
